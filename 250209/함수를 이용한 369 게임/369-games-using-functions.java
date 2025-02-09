5268aab1fb567905df5dbfd0ee8181196e28729e
JunYoungMoon <35192352+JunYoungMoon@users.noreply.github.com>
2025-02-09 15:12:29 +0900

    Title: 함수를 이용한 369 게임, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250209/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean containsMultipleOfThree(int n) {
+        if ((n % 10) % 3 == 0 || (n / 10) % 3 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isMagicMultipleOfThree(int n) {
+        if (n % 3 == 0 || containsMultipleOfThree(n)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int cnt = 0;
+
+        for(int i = A ; i <= B; i++){
+            if(isMagicNumber(i)){
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
