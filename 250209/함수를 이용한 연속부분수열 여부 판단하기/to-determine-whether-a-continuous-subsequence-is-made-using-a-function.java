7f783bb0dea993af00621a50d31494f418fb2187
JunYoungMoon <35192352+JunYoungMoon@users.noreply.github.com>
2025-02-09 23:38:50 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 121ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250209/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String hasSubarrayWithSum(int[] arrA, int[] arrB, int sumB) {
+        int sumA = 0;
+
+        for (int i = 0; i < arrB.length; i++) {
+            sumA = sumA + arrA[i];
+        }
+
+        if(sumA == sumB){
+            return "Yes";
+        }
+
+        for (int i = arrB.length; i < arrA.length; i++) {
+            sumA = sumA + arrA[i];
+            sumA = sumA - arrA[i - arrB.length];
+
+            if(sumA == sumB){
+                return "Yes";
+            }
+        }
+
+        return "No";
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lenA = scanner.nextInt();
+        int lenB = scanner.nextInt();
+
+        int[] arrA = new int[lenA];
+        int[] arrB = new int[lenB];
+
+        for (int i = 0; i < lenA; i++) {
+            arrA[i] = scanner.nextInt();
+        }
+
+        scanner.nextLine();
+
+        int sumB = 0;
+        for (int i = 0; i < lenB; i++) {
+            arrB[i] = scanner.nextInt();
+            sumB = sumB + arrB[i];
+        }
+
+        System.out.println(hasSubarrayWithSum(arrA, arrB, sumB));
+    }
+}
