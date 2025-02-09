cb5e5760b4804bf9665d03570c50b442d99e4663
JunYoungMoon <35192352+JunYoungMoon@users.noreply.github.com>
2025-02-09 20:54:45 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 250209/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
+++ 250209/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static boolean 소수구하기(int num){
+    public static boolean Prime(int num){
         for(int i = 2; i < num; i++){
             if(num % i == 0){
                 return false;
             }
         }
-        
-        if(자릿수합이짝수인가(num)){
+
+        if(DigitsSum(num)){
             return true;
         }
 
         return false;
     }
 
-    public static boolean 자릿수합이짝수인가(int num){
+    public static boolean DigitsSum(int num){
         int sum = 0;
         while(num > 0){
             sum = sum + num % 10;
@@ -34,7 +34,7 @@ public class Main {
         int answer = 0;
 
         for(int i = A; i <= B ; i++){
-            if(소수구하기(i)){
+            if(Prime(i)){
                 answer++;
             }
         }
