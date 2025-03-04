6257acf6c16b27344171c1cebe86b750de64c5f2
JunYoungMoon <35192352+JunYoungMoon@users.noreply.github.com>
2025-03-04 11:51:39 +0900

    Title: 좌우로 움직이는 로봇, Time: 778ms, Memory: 41MB, Status: Runtime Error - CodeTree

--- 250304/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250304/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Main {
 
-    public static int[][] posN = new int[1000000][2];
-    public static int[][] posM = new int[1000000][2];
+    public static int[] posN = new int[1000000];
+    public static int[] posM = new int[1000000];
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -12,20 +12,15 @@ public class Main {
         int m = scanner.nextInt();
 
         int timeA = 1;
-        int distanceA = 0;
         for (int i = 0; i < n; i++) {
             int t = scanner.nextInt();
-            String d = scanner.next();
+            char d = scanner.next().charAt(0);
 
             for (int j = 0; j < t; j++) {
-                if (Objects.equals(d, "L")) {
-                    distanceA--;
-                    posN[timeA][0] = distanceA;
-                    posN[timeA][1] = -1;
-                } else if (Objects.equals(d, "R")) {
-                    distanceA++;
-                    posN[timeA][0] = distanceA;
-                    posN[timeA][1] = 1;
+                if (d == 'L') {
+                    posN[timeA] = posN[timeA - 1] - 1;
+                } else if (d == 'R') {
+                    posN[timeA] = posN[timeA - 1] + 1;
                 }
 
                 timeA++;
@@ -33,20 +28,15 @@ public class Main {
         }
 
         int timeB = 1;
-        int distanceB = 0;
         for (int i = 0; i < m; i++) {
             int t = scanner.nextInt();
-            String d = scanner.next();
+            char d = scanner.next().charAt(0);
 
             for (int j = 0; j < t; j++) {
-                if (Objects.equals(d, "L")) {
-                    distanceB--;
-                    posM[timeB][0] = distanceB;
-                    posM[timeB][1] = -1;
-                } else if (Objects.equals(d, "R")) {
-                    distanceB++;
-                    posM[timeB][0] = distanceB;
-                    posM[timeB][1] = 1;
+                if (d == 'L') {
+                    posM[timeB] = posM[timeB - 1] - 1;
+                } else if (d == 'R') {
+                    posM[timeB] = posM[timeB - 1] + 1;
                 }
 
                 timeB++;
@@ -64,15 +54,10 @@ public class Main {
         }
 
         int ans = 0;
-        int timeMax;
-        if (timeA < timeB) {
-            timeMax = timeB;
-        } else {
-            timeMax = timeA;
-        }
+        int timeMax = Math.max(timeA, timeB);
 
         for (int i = 1; i < timeMax; i++) {
-            if (posN[i][0] == posM[i][0] && posN[i][1] != posM[i][1]) {
+            if (posN[i] == posM[i] && posN[i - 1] != posM[i - 1]) {
                 ans++;
             }
         }
