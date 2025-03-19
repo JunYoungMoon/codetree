3b662595a20cfeb95cb43ea207f43109d62fc853
JunYoungMoon <35192352+JunYoungMoon@users.noreply.github.com>
2025-03-19 10:56:17 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 127ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250319/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] dx = new int[]{1, 0, -1, 0};
+    public static int[] dy = new int[]{0, -1, 0, 1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+
+        char[] cArr = new char[t];
+
+        String str = sc.next();
+
+        for (int i = 0; i < t; i++) {
+           cArr[i]  = str.charAt(i);
+        }
+
+        int x = n / 2;
+        int y = n / 2;
+
+        int dir = 2;
+
+        int[][] grid = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+            sc.nextLine();
+        }
+
+        int ans = grid[x][y];
+        for (char c : cArr) {
+            if (c == 'F') {
+                //해당 방향으로 한칸 이동
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                    x = nx;
+                    y = ny;
+                    ans += grid[nx][ny];
+                }
+            } else if (c == 'R') {
+                //오른쪽 방향만 회전
+                dir = (dir + 1) % 4;
+            } else if (c == 'L') {
+                //왼쪽 방향만 회전
+                dir = (dir + 3) % 4;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
