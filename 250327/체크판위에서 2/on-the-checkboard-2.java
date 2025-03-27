fc69a07871a8dfc82132b27a4a9bff7bdd00d52b
JunYoungMoon <35192352+JunYoungMoon@users.noreply.github.com>
2025-03-27 11:02:17 +0900

    Title: 체크판위에서 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250327/체크판위에서 2/on-the-checkboard-2.java
+++ 250327/체크판위에서 2/on-the-checkboard-2.java
@@ -10,7 +10,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 입력
         n = sc.nextInt();
         m = sc.nextInt();
         for(int i = 0; i < n; i++)
@@ -23,7 +22,6 @@ public class Main {
             for(int j = 1; j < m; j++)
                 for(int k = i + 1; k < n - 1; k++)
                     for(int l = j + 1; l < m - 1; l++)
-                        // 그 중 색깔이 전부 달라지는 경우에만 개수를 세줍니다.
                         if(grid[i][j] != grid[k][l])
                             cnt++;
 
