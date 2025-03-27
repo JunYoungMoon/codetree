2fa17dbeb5bc58ce49994df1e633412b8741c41f
JunYoungMoon <35192352+JunYoungMoon@users.noreply.github.com>
2025-03-27 11:03:04 +0900

    Title: 체크판위에서 2, Time: 114ms, Memory: 10MB, Status: Passed - Codetree

--- 250327/체크판위에서 2/on-the-checkboard-2.java
+++ 250327/체크판위에서 2/on-the-checkboard-2.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static final int MAX_N = 15;
 
     public static int n, m;
     public static char[][] grid = new char[MAX_N][MAX_N];
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -22,9 +21,11 @@ public class Main {
             for(int j = 1; j < m; j++)
                 for(int k = i + 1; k < n - 1; k++)
                     for(int l = j + 1; l < m - 1; l++)
-                        if(grid[i][j] != grid[k][l])
+                        if(grid[0][0] != grid[i][j] && 
+                           grid[i][j] != grid[k][l] &&
+                           grid[k][l] != grid[n - 1][m - 1])
                             cnt++;
-
+        
         System.out.println(cnt);
     }
 }
