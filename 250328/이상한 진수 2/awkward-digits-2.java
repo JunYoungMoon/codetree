8df88f3657fdda908c7aa3f1d1870cd9f8d8d7f6
JunYoungMoon <35192352+JunYoungMoon@users.noreply.github.com>
2025-03-28 11:00:57 +0900

    Title: 이상한 진수 2, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250328/이상한 진수 2/awkward-digits-2.java
+++ 250328/이상한 진수 2/awkward-digits-2.java
@@ -4,16 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String binary = sc.next();
-
-        for (int i = 0; i < binary.length(); i++) {
-            if (binary.charAt(i) == '0') {
-                binary = binary.substring(0, i) + '1' + binary.substring(i + 1);
-                break;
+        
+        // 만약 모든 숫자가 '1'이면 가장 큰 값을 위해 첫 번째 1을 0으로 변경
+        if (!binary.contains("0")) {
+            binary = "0" + binary.substring(1); // 첫 번째 숫자를 0으로 바꿈
+        } else {
+            // 가장 앞의 '0'을 '1'로 변경
+            for (int i = 0; i < binary.length(); i++) {
+                if (binary.charAt(i) == '0') {
+                    binary = binary.substring(0, i) + '1' + binary.substring(i + 1);
+                    break; // 첫 번째 0을 변경한 후 종료
+                }
             }
         }
 
+        // 변환된 이진수를 십진수로 변환
         int num = Integer.parseInt(binary, 2);
 
+        // 출력
         System.out.println(num);
     }
 }
