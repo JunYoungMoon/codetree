f52708b25933b55e112f24a407c1812f89e1daf8
JunYoungMoon <35192352+JunYoungMoon@users.noreply.github.com>
2025-03-29 22:11:04 +0900

    Title: 이상한 진수 2, Time: 137ms, Memory: 10MB, Status: Passed - Codetree

--- 250329/이상한 진수 2/awkward-digits-2.java
+++ 250329/이상한 진수 2/awkward-digits-2.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int INT_MIN = Integer.MIN_VALUE;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String binary = sc.next();
-        
-        // 만약 모든 숫자가 '1'이면 가장 큰 값을 위해 첫 번째 1을 0으로 변경
-        if (!binary.contains("0")) {
-            binary = "0" + binary.substring(1); // 첫 번째 숫자를 0으로 바꿈
-        } else {
-            // 가장 앞의 '0'을 '1'로 변경
-            for (int i = 0; i < binary.length(); i++) {
-                if (binary.charAt(i) == '0') {
-                    binary = binary.substring(0, i) + '1' + binary.substring(i + 1);
-                    break; // 첫 번째 0을 변경한 후 종료
-                }
-            }
+
+        int ans = INT_MIN;
+        for(int i = 0; i < binary.length(); i++) {
+            //(char)('0' + '1' - binary.charAt(i)) i번째 숫자 반대로 뒤집기
+            binary = binary.substring(0, i) + (char)('0' + '1' - binary.charAt(i)) + binary.substring(i + 1);
+            int num = 0;
+            for(int j = 0; j < binary.length(); j++)
+                num = num * 2 + (binary.charAt(j) - '0');
+
+            ans = Math.max(ans, num);
+
+            binary = binary.substring(0, i) + (char)('0' + '1' - binary.charAt(i)) + binary.substring(i + 1);
         }
 
-        // 변환된 이진수를 십진수로 변환
-        int num = Integer.parseInt(binary, 2);
-
-        // 출력
-        System.out.println(num);
+        System.out.print(ans);
     }
 }
