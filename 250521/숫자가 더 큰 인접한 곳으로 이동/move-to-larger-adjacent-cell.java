54b133bb6656e86ef09afdde814a4d01050f79f9
JunYoungMoon <35192352+JunYoungMoon@users.noreply.github.com>
2025-05-21 15:00:35 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 158ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 250521/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] dx = {0, 1, -1, 0};
+        int[] dy = {-1, 0, 0, 1};
+
+        int n = sc.nextInt();
+        int x = sc.nextInt() - 1;
+        int y = sc.nextInt() - 1;
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        int currentValue = arr[x][y];
+        list.add(currentValue);
+        int cnt = 1;
+        int currentX = 0;
+        int currentY = 0;
+
+        while (cnt > 0) {
+            cnt = 0;
+            for (int i = 0; i < 4; i++) {
+                currentX = x + dx[i];
+                currentY = y + dy[i];
+                if (currentX >= 0 && currentX < n && currentY >= 0 && currentY < n) {
+                    if (arr[currentX][currentY] > currentValue) {
+                        currentValue = arr[currentX][currentY];
+                        list.add(currentValue);
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            x = currentX;
+            y = currentY;
+        }
+
+        for (int value : list) {
+            System.out.print(value + " ");
+        }
+    }
+}
