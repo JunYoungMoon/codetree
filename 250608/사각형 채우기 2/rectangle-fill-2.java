b2f9509eb4bcf1a795d03038a175803b77e3aaea
JunYoungMoon <35192352+JunYoungMoon@users.noreply.github.com>
2025-06-08 10:44:07 +0900

    Title: 사각형 채우기 2, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250608/사각형 채우기 2/rectangle-fill-2.java
+++ 250608/사각형 채우기 2/rectangle-fill-2.java
@@ -18,7 +18,7 @@ public class Main {
         dp[2] = 3;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] * 1) + (dp[i - 2] * 2) % mod;
+            dp[i] = ((dp[i - 1] * 1) + (dp[i - 2] * 2)) % mod;
         }
 
         System.out.println(dp[n]);
